b9355d3f9d7b9ca33178366b1c0eae92272503ad
albiant <98alby@gmail.com>
2021-10-25 17:53:01 +0200

    Available Devices
    New activity

---
+++ app/src/main/java/org/acastronovo/tesi/AvailableDevices.java
@@ -0,0 +1,14 @@
+package org.acastronovo.tesi;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AvailableDevices extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_available_devices);
+    }
+}
