afd4d0e686d3dbce521a2f5b46788aac6f23af47
albiant <98alby@gmail.com>
2021-11-16 11:23:51 +0100

    Funzione subscribe
    Implementazione del metodo subscribe nel file java received data

--- app/src/main/java/org/acastronovo/tesi/ReceivedData.java
+++ app/src/main/java/org/acastronovo/tesi/ReceivedData.java
@@ -1,6 +1,8 @@
 package org.acastronovo.tesi;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
 import android.os.Bundle;
 import android.annotation.SuppressLint;
 import android.app.Service;
@@ -11,6 +13,7 @@ import android.content.IntentFilter;
 import android.os.IBinder;
 import android.os.Message;
 import android.util.Log;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import org.eclipse.paho.android.service.MqttAndroidClient;
@@ -30,16 +33,74 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.nio.charset.StandardCharsets;
-
-
+import java.util.Objects;
 
 
 public class ReceivedData extends AppCompatActivity {
+
+    //Toolbar
+    Toolbar toolbar;
+
+    //UI
+    TextView temperature;
+    TextView heartbeat;
+    TextView humidity;
+    TextView position;
+    TextView altitude;
+    TextView pressure;
+    TextView pedometer;
+    TextView calories;
+
+    MqttAsyncClient client;
+    String TAG = "MqttService";
+
+    String topic = "  ";
+    int qos = 0;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_received_data);
+
+        //Toolbar
+        toolbar = findViewById(R.id.toolbar_device_info);
+        setSupportActionBar(toolbar);
+        Objects.requireNonNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
+
+        //UI
+        temperature= findViewById(R.id.temperature);
+        heartbeat= findViewById(R.id.heartbeat);
+        humidity = findViewById(R.id.humidity);
+        position = findViewById(R.id.position);
+        altitude = findViewById(R.id.altitude);
+        pressure = findViewById(R.id.pressure);
+        pedometer = findViewById(R.id.pedometer);
+        calories = findViewById(R.id.calories);
+
+        Intent intent= getIntent();
+        String client=intent.get(ConnectedDevices.client);
+
+        try {
+            IMqttToken subToken = client.subscribe(topic, qos);
+            subToken.setActionCallback(new IMqttActionListener() {
+                @Override
+                public void onSuccess(IMqttToken asyncActionToken) {
+                    // The message was published
+                    Log.d(TAG, "The message was published");
+                }
+
+                @Override
+                public void onFailure(IMqttToken asyncActionToken,
+                                      Throwable exception) {
+                    // The subscription could not be performed, maybe the user was not
+                    // authorized to subscribe on the specified topic e.g. using wildcards
+                    Log.d(TAG, "The subscription could not be performed");
+                }
+            });
+        } catch (MqttException e) {
+            e.printStackTrace();
+        }
 
 
 
